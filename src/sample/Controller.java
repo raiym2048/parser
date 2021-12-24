0ba7b8ec833bd7c2add1794e5daa778422f9c998
Mirlan <mirlandaniaruulu19@gmail.com>
2021-12-24 21:23:47 +0600

    added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,44 +1,42 @@
 package sample;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import org.json.JSONObject;
-
-import javax.xml.soap.Text;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.json.JSONObject;
+
 public class Controller {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
 
     @FXML
     private TextField city;
 
     @FXML
+    private Text simple_text;
+
+    @FXML
     private Button start_weather;
 
     @FXML
-    private Text temp;
+    private Text temp_info;
 
-    @FXML//sample.Controller
 
-    void initialize() {
+    public void initialize() {
         start_weather.setOnAction(event -> {
             String getUserCity = city.getText().trim();
 if (!getUserCity.equals("")) {
                 String output = getUrlContent("https://api.openweathermap.org/data/2.5/weather?q=" + getUserCity + "&appid=f51990e7149906aa689774767bf390da&unit=metric");
                 if (!output.isEmpty()) {
                     JSONObject obj = new JSONObject(output);
-                    temp.setData("температура: " + obj.getJSONObject("main").getDouble("temp"));
+                    double res = Math.floor(obj.getJSONObject("main").getDouble("temp")-273.15f);
+                    temp_info.setText("Temperature is : " + res + "°C");
+                    System.out.println(res);
                 }
             }
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,9 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 
@@ -17,22 +14,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.setScene(new Scene(root, 600, 700));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-        /*String url = "https://www.gismeteo.by/weather-bishkek-5327/10-days/";
-
-        try {
-            Document d = Jsoup.connect(url).get();
-           *//* for(Element row : d.select(""));*//*
-            System.out.println(d.outerHtml());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
         launch(args);
     }
 }
